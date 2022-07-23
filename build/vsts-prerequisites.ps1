6403a6a9d8485aabffddfd5fe818d16261884190
Andi Bellstedt <bellstedt@gmx.de>
2022-07-23 21:32:41 +0200

    explicit pester version for testing

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -3,7 +3,7 @@
     $Repository = 'PSGallery'
 )
 
-$modules = @("Pester", "PSFramework", "PSModuleDevelopment", "PSScriptAnalyzer")
+$modules = @("PSFramework", "PSModuleDevelopment", "PSScriptAnalyzer")
 
 # Automatically add missing dependencies
 $data = Import-PowerShellDataFile -Path "$PSScriptRoot\..\WinEventLogCustomization\WinEventLogCustomization.psd1"
@@ -23,3 +23,7 @@ foreach ($module in $modules) {
     Install-Module $module -Force -SkipPublisherCheck -Repository $Repository
     Import-Module $module -Force -PassThru
 }
+
+Write-Host "Installing Pester v5.3.1" -ForegroundColor Cyan
+Install-Module -Name "Pester" -RequiredVersion 5.3.1 -Force -SkipPublisherCheck -Repository $Repository
+Import-Module "Pester" -Force -PassThru
